014bd916f8d51a4b92186f7aabdb280853baf825
Meghashyam Timmireddy <mtimmireddy@gmail.com>
2019-02-25 17:56:37 -0600

    initial commit

---
+++ src/main/java/com/app/StationFinder/StationFinderApplication.java
@@ -0,0 +1,13 @@
+package com.app.StationFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationFinderApplication.class, args);
+	}
+
+}
